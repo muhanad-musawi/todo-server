a1086c9b0da1a24ab757981c5e668c542c1897cd
muhanad-musawi <muhanad-al-musawi@hotmail.com>
2023-01-17 18:43:20 +0100

    added new test

--- src/todos/todos.test.ts
+++ src/todos/todos.test.ts
@@ -1,11 +1,18 @@
 import fs from 'fs';
-import { saveTodo } from './todos';
+import { toUnicode } from 'punycode';
+import { saveTodo, getTodos} from './todos';
+beforeEach(() => { 
+    const exists = fs.existsSync('./todos.json')
+    if (exists) {
+        fs.unlinkSync('./todos.json')
+    }
+})
 
 test("if todo.json file is created", () => {
     // given / gegeben
     // es gibt noch keine todos.json
     // testen obs da ist / bzw. löschen
-
+    
     // when / wenn 
     // ich ein Todo Speicher
     saveTodo("Hallo Welt")
@@ -14,12 +21,76 @@ test("if todo.json file is created", () => {
     // muss eine json datei existieren
     const exists = fs.existsSync('./todos.json')
     expect(exists).toBeTruthy()
+
 })
 
-// test("speichert valides json")
-// test("")
+ test("speichert valides json", () => {
+    saveTodo("Hallo Welt")
+   
+    const content = fs.readFileSync('./todos.json', 'utf-8' )
+    expect(() => JSON.parse(content)).not.toThrow()
+})
+
+// neu hinzugefügt
+test("speichert ein Todo ", () => {
+    saveTodo("Hallo Welt")
+    const content = fs.readFileSync('./todos.json', 'utf-8' )
+    expect(content).toContain("Hallo Welt")
+})
+
+// neu hinzugefügt
+test("speichert den status false", () => {
+    saveTodo("Hallo Welt")
+    const content = fs.readFileSync('./todos.json', 'utf-8' )
+    expect(content).toContain("false")
+})
+
+
+// --- Lesen
+
+test("Liest die json datei und gibt ein Array zurück", () => {
+    saveTodo("test")
+    const todos = getTodos();
+    
+    expect(Array.isArray(todos)).toBe(true);
+})
+
+test("Schreib ein Todo in die json Datei und lese es wieder raus", () => {
+    saveTodo("Test")
+    const todos = getTodos(); 
+    
+    expect(todos.length).toBe(1);
+    expect(todos[0].title).toBe("Test");
+})
+
+    
+/*
+test("doppelt eingetragen", () => { 
+    // given / gegeben
+    const content = fs.readFileSync('./todos.json')
+ 
+    expect(content).not.toContain("Hallo Welt")
+    expect(new Set(content)).not.toContain("Hallo Welt");
+   // expect(content).not.toHaveReturnedWith("Hallo Welt");
+})
 
 // --- abhakt
+test(" ToDo check exists", () => {
+    // given / gegeben
+    saveCheck("Hallo Welt", true)
 
+    // then / dann
+    const exists = fs.existsSync('./todos.json')
+    expect(exists).toBeTruthy()
+})
 
+test("check is true", () => {
+    // given / gegeben
+    // when / wenn 
+    // then / dann
+    const content = fs.readFileSync('./todos.json', 'utf-8' )
+    expect(content).toContain("true")
+})
+*/
 // --- anzeigt
+
--- src/todos/todos.ts
+++ src/todos/todos.ts
@@ -1,5 +1,17 @@
 import fs from 'fs';
 
 export function saveTodo(title: string) {
-    fs.writeFileSync("./todos.json", title)
+    const todos = [{title, status: false}]
+    fs.writeFileSync("./todos.json", JSON.stringify(todos) ) 
+}
+
+export function getTodos() : Array<{title: string, status: boolean}> { 
+    
+    const fileContent = fs.readFileSync("./todos.json", "utf-8")
+    const todos = JSON.parse(fileContent)
+    return todos
+}
+
+export function saveCheck(title: string, check: boolean) {
+    fs.writeFileSync("./todos.json", title + check)
 }
