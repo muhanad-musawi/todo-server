319ee6b64ae85f66377848bc09f12be2f52cba05
muhanad-musawi <al-musawi-muhanad@hotmail.com>
2023-01-19 15:27:52 +0100

    import controller in router.ts

--- src/todos/routes/todoRouter.ts
+++ src/todos/routes/todoRouter.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import {
-    saveTodo,
-    getTodos,
-    filterCheckTodos,
-    filterFavouriteTodos,
-} from '../todos';
+    saveTodos,
+    getAllTodos,
+    getCheckTodos,
+    getFavouriteTodos,
+} from '../controller/todosController';
 
 const router = Router();
 
-router.route('/get-todos').get(getTodos);
+router.route('/get-todos').get(getAllTodos);
 // localhost:4000/get-todos/ -> Endpunkt
 
-router.route('/save-todo').post(saveTodo);
+router.route('/save-todo').post(saveTodos);
 // localhost:4000/save-todo/ -> Endpunkt
 
-router.route('/filter-check-todos').get(filterCheckTodos);
+router.route('/filter-check-todos').get(getCheckTodos);
 // localhost:4000/filter-check-todos/ -> Endpunkt
 
-router.route('/filter-favourite-todos').get(filterFavouriteTodos);
+router.route('/filter-favourite-todos').get(getFavouriteTodos);
 // localhost:4000/filter-favourite-todos/ -> Endpunkt
 
 export default router;
